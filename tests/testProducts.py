d0c35ddd72e8550007233d7e6765d2b6982468ce
Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
2023-11-17 11:20:39 +0100

    Simplify testing in testProducts.py
    Co-Authored-By: Tore Forsberg <142976914+Tore-Forsberg@users.noreply.github.com>
    Co-Authored-By: Johannes <65119813+Johannes29@users.noreply.github.com>

--- tests/testProducts.py
+++ tests/testProducts.py
@@ -1,11 +1,9 @@
 # !IMPORTANT!
 # Run startServer.py before running this test
 
-import csv
 import unittest
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 optionsChrome = webdriver.ChromeOptions()  # Define options for chrome
 optionsChrome.add_argument("headless")  # Pass headless argument to the options (no ui)
@@ -13,8 +11,6 @@ browser = webdriver.Chrome(options=optionsChrome)
 
 # Adress to website
 website = "http://127.0.0.1:8000/?products=testProductList.csv&images=testImageList.csv&openHours=testOpenHoursListOpen.csv"
-# Path to product list csv file
-productListPath = "public/csv/testProductList.csv"
 # Resolution on screen
 res = 1080, 1920
 
@@ -27,35 +23,15 @@ class TestProducts(unittest.TestCase):
         browser.get(website)
         browser.set_window_size(*res)
 
-    def testProductList(self):
-        # Creates an array with the product data from the csv file
-        menuList = []
+    def testVisibleProducts(self):
+        self.assertIn("Mazariner", self.browser.page_source)
+        self.assertIn("Kycklingröra", self.browser.page_source)
+        self.assertIn("Tekaka Kalkon/ost eller ost", self.browser.page_source)
 
-        with open(productListPath, encoding="utf-8", newline="") as products:
-            lines = csv.reader(products, delimiter=",", quotechar="|")
-            for row in lines:
-                menuList.append(row)
-
-        # Loops through every category that starts at multiple of 4 (in list index)
-        for categoryIndex in range(0, round(len(menuList)), 4):
-            categoryVisibilityList = menuList[2 + categoryIndex]
-            categoryItemList = menuList[0 + categoryIndex]
-
-            # Loops through the current category, starting at 1 to only include the relevant data
-            for itemIndex in range(1, len(categoryVisibilityList)):
-                # Finds an element that contains the wanted text while not part of an image slide
-                xpath = f"//p[contains(text(), '{categoryItemList[itemIndex]}') and not(contains(@class,'itemText'))]"
-                item = self.browser.find_elements(By.XPATH, xpath)
-
-                if categoryVisibilityList[itemIndex] == "TRUE":
-                    # If the element can not be found when it should the test fails
-                    if len(item) == 0:
-                        raise Exception(categoryItemList[itemIndex] + " was not found")
-
-                elif categoryVisibilityList[itemIndex] == "FALSE":
-                    # If the element can be found when it should not the test fails
-                    if not len(item) == 0:
-                        raise Exception(categoryItemList[itemIndex] + " was found")
+    def testHiddenProducts(self):
+        self.assertNotIn("Kiviks Apelsin/Äpple", self.browser.page_source)
+        self.assertNotIn("Kalkon-Ost", self.browser.page_source)
+        self.assertNotIn("Marabou choklad", self.browser.page_source)
 
     # Closes the window after all the tests are done
     @classmethod
