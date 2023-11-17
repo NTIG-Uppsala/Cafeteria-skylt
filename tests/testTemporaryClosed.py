d764cb4d9bf1e992de8f7d833fc3ade42c5d3e18
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-11-17 15:51:57 +0100

    Integrate base test class in testTemporaryClosed.py
    Co-Authored-By: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
    Co-Authored-By: Tore Forsberg <142976914+Tore-Forsberg@users.noreply.github.com>

--- tests/testTemporaryClosed.py
+++ tests/testTemporaryClosed.py
@@ -1,30 +1,15 @@
 # !IMPORTANT!
 # Run startServer.py before running this test
 
-import csv
 import unittest
 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-optionsChrome = webdriver.ChromeOptions()  # Define options for chrome
-optionsChrome.add_argument("headless")  # Pass headless argument to the options (no ui)
-browser = webdriver.Chrome(options=optionsChrome)
-
-# Adress to website
-website = "http://127.0.0.1:8000/?products=testProductList.csv&images=testImageList.csv&openHours=testOpenHoursListClosed.csv"
-# Resolution on screen
-res = 1080, 1920
+from baseTestClass import BaseTestClass
 
 
-# Runs tests in Chrome
-class TestTemporaryClosed(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        self.browser = browser
-        browser.get(website)
-        browser.set_window_size(*res)
-
+class TestTemporaryClosed(BaseTestClass):
+    website = "http://127.0.0.1:8000/?products=testProductList.csv&images=testImageList.csv&openHours=testOpenHoursListClosed.csv"
     def testTemporaryClosedSlidesCount(self):
         slides = self.browser.find_elements(By.CLASS_NAME, "carousel-item")
         # Only one slide should exist when temporary closed
@@ -32,11 +17,6 @@ class TestTemporaryClosed(unittest.TestCase):
 
     def testTemporaryClosedText(self):
         self.assertIn("Cafeterian har tillfälligt stängt", self.browser.page_source)
-
-    # Closes the window after all the tests are done
-    @classmethod
-    def tearDownClass(self):
-        self.browser.close()
 
 
 # Starts test if run as python file
