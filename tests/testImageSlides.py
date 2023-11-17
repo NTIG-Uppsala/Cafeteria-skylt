ebcd9e43693a1bb2cc260ae67ffe593bf18085f1
Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
2023-11-17 11:28:26 +0100

    Simplify testing in testImagesSlides.py
    Co-Authored-By: Tore Forsberg <142976914+Tore-Forsberg@users.noreply.github.com>
    Co-Authored-By: Johannes <65119813+Johannes29@users.noreply.github.com>

--- tests/testImageSlides.py
+++ tests/testImageSlides.py
@@ -1,11 +1,9 @@
 # !IMPORTANT!
 # Run startServer.py before running this test
 
-import csv
 import unittest
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 optionsChrome = webdriver.ChromeOptions()  # Define options for chrome
 optionsChrome.add_argument("headless")  # Pass headless argument to the options (no ui)
@@ -13,8 +11,6 @@ browser = webdriver.Chrome(options=optionsChrome)
 
 # Adress to website
 website = "http://127.0.0.1:8000/?products=testProductList.csv&images=testImageList.csv&openHours=testOpenHoursListOpen.csv"
-# Path to product list csv file
-imageListPath = "public/csv/testImageList.csv"
 # Resolution on screen
 res = 1080, 1920
 
@@ -28,36 +24,13 @@ class TestImages(unittest.TestCase):
         browser.set_window_size(*res)
 
     def testImagesPresent(self):
-        # Creates an array with data for the image slides from the csv file
-        imageDataList = []
+        self.assertIn("toast.png", self.browser.page_source)
+        self.assertIn("danishPastry.png", self.browser.page_source)
 
-        with open(imageListPath, encoding="utf-8", newline="") as images:
-            lines = csv.reader(images, delimiter=",", quotechar="|")
-            for row in lines:
-                imageDataList.append(row)
+    def testImagesHidden(self):
+        self.assertNotIn("coffee.png", self.browser.page_source)
+        self.assertNotIn("cinnamonBun.png", self.browser.page_source)
 
-        imageVisibilityList = imageDataList[4]
-        imagePathList = imageDataList[2]
-
-        # Loops through the image data, starting at 1 to only include the relevant data
-        for imageIndex in range(1, len(imageVisibilityList)):
-            # Finds an image element containing the desired image source
-            xpath = f"//img[@src = 'images/products/{imagePathList[imageIndex]}']"
-            image = self.browser.find_elements(By.XPATH, xpath)
-
-            if imageVisibilityList[imageIndex] == "TRUE":
-                # If an element with the image source can not be found when it should the test fails
-                if len(image) == 0:
-                    raise Exception(
-                        f"An image with the source: {imagePathList[imageIndex]} was not found"
-                    )
-
-            elif imageVisibilityList[imageIndex] == "FALSE":
-                # If an element with the image source can be found when it should not the test fails
-                if not len(image) == 0:
-                    raise Exception(
-                        f"An image with the source: {imagePathList[imageIndex]} was found"
-                    )
 
     # Closes the window after all the tests are done
     @classmethod
