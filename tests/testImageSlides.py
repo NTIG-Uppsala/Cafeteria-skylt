e214a8402db52a3301666d3d07424dfcc7f546be
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-11-17 15:20:22 +0100

    Integrate base test class in testImageSlides.py
    Co-Authored-By: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>

--- tests/testImageSlides.py
+++ tests/testImageSlides.py
@@ -3,26 +3,10 @@
 
 import unittest
 
-from selenium import webdriver
-
-optionsChrome = webdriver.ChromeOptions()  # Define options for chrome
-optionsChrome.add_argument("headless")  # Pass headless argument to the options (no ui)
-browser = webdriver.Chrome(options=optionsChrome)
-
-# Adress to website
-website = "http://127.0.0.1:8000/?products=testProductList.csv&images=testImageList.csv&openHours=testOpenHoursListOpen.csv"
-# Resolution on screen
-res = 1080, 1920
+from baseTestClass import BaseTestClass
 
 
-# Runs tests in Chrome
-class TestImages(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        self.browser = browser
-        browser.get(website)
-        browser.set_window_size(*res)
-
+class TestImageSlides(BaseTestClass):
     def testImagesPresent(self):
         self.assertIn("toast.png", self.browser.page_source)
         self.assertIn("danishPastry.png", self.browser.page_source)
@@ -30,12 +14,6 @@ class TestImages(unittest.TestCase):
     def testImagesHidden(self):
         self.assertNotIn("coffee.png", self.browser.page_source)
         self.assertNotIn("cinnamonBun.png", self.browser.page_source)
-
-
-    # Closes the window after all the tests are done
-    @classmethod
-    def tearDownClass(self):
-        self.browser.close()
 
 
 # Starts test if run as python file
