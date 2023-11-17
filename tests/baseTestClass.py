2d86f478cd9762b90e9a8f2a5d76e18990abd954
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-11-17 15:06:09 +0100

    Add base class for tests
    Co-Authored-By: Tore Forsberg <142976914+Tore-Forsberg@users.noreply.github.com>
    Co-Authored-By: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>

---
+++ tests/baseTestClass.py
@@ -0,0 +1,28 @@
+from unittest import TestCase, main
+
+from selenium import webdriver
+
+
+class BaseTestClass(TestCase):
+    doNotCloseBrowser = False
+    hideWindow = True
+    website = "http://127.0.0.1:8000/?products=testProductList.csv&images=testImageList.csv&openHours=testOpenHoursListOpen.csv"
+    resolution = 1080, 1920
+
+    @classmethod
+    def setUpClass(cls):
+        chromeOptions = webdriver.ChromeOptions()
+
+        if cls.doNotCloseBrowser:
+            chromeOptions.add_experimental_option("detach", True)
+
+        if cls.hideWindow:
+            chromeOptions.add_argument("--headless")
+
+        cls.browser = webdriver.Chrome(options=chromeOptions)
+        cls.browser.get(cls.website)
+        cls.browser.set_window_size(*cls.resolution)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.close()
