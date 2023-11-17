f1b74670236d6ed20523cc260511d39a93be2df9
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-11-17 14:30:30 +0100

    Simplify testTemporaryClosed test
    Co-Authored-By: Tore Forsberg <142976914+Tore-Forsberg@users.noreply.github.com>
    Co-Authored-By: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>

--- tests/testTemporaryClosed.py
+++ tests/testTemporaryClosed.py
@@ -7,8 +7,6 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-import time
-
 optionsChrome = webdriver.ChromeOptions()  # Define options for chrome
 optionsChrome.add_argument("headless")  # Pass headless argument to the options (no ui)
 browser = webdriver.Chrome(options=optionsChrome)
@@ -28,21 +26,9 @@ class TestTemporaryClosed(unittest.TestCase):
         browser.set_window_size(*res)
 
     def testTemporaryClosed(self):
-        # Checks that the closed slide is the only slide active every 5 seconds for 25 seconds
-        for i in range(5):
-            slides = self.browser.find_elements(By.CLASS_NAME, "carousel-item")
-            if len(slides) > 1:
-                raise Exception("Additional slides are active")
-            time.sleep(5)
-
-    def testTemporaryClosedWithRefresh(self):
-        # Checks that the closed slide is the only slide active 10 times with 2 seconds delay
-        for i in range(10):
-            slides = self.browser.find_elements(By.CLASS_NAME, "carousel-item")
-            if len(slides) > 1:
-                raise Exception("Additional slides are active")
-            self.browser.execute_script("location.reload();")
-            time.sleep(2)
+        slides = self.browser.find_elements(By.CLASS_NAME, "carousel-item")
+        # Only one slide should exist when temporary closed
+        self.assertEqual(len(slides), 1)
 
     # Closes the window after all the tests are done
     @classmethod
