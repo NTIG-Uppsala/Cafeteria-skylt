0af7580c94e59231d93875e8b1b6660d4b42c758
Georg af Wåhlberg <105862332+Georg-af-W@users.noreply.github.com>
2023-11-23 11:07:26 +0100

    Disable cache in startServer.py

--- startServer.py
+++ startServer.py
@@ -3,12 +3,22 @@ import platform
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from os import getcwd, path
 
+class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_my_headers()        
+        SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_my_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+
 if platform.system() == "Windows":
     cwd = getcwd()
     os.chdir(cwd + "/public")
 elif platform.system() == "Linux":
     os.chdir("/home/pi/Git/cafeteria-display/public")
 serverAddress = ("", 8000)
-httpd = HTTPServer(serverAddress, SimpleHTTPRequestHandler)
+httpd = HTTPServer(serverAddress, MyHTTPRequestHandler)
 print("\nThe server is open at http://127.0.0.1:8000")
 httpd.serve_forever()
