a0963e7e2689fb6c292f6c72da83039f9fafe238
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-11-23 09:38:44 +0100

    Add empty rows to imageSlideCreator.js
    Co-Authored-By: Tore Forsberg <142976914+Tore-Forsberg@users.noreply.github.com>
    Co-Authored-By: Georg af Wåhlberg <105862332+georg-af-w@users.noreply.github.com>

--- public/js/imageSlideCreator.js
+++ public/js/imageSlideCreator.js
@@ -18,27 +18,35 @@ function helperGetImageSlide(data){
             imageSlide.className = "carousel-item slide";
             imageSlide.setAttribute("data-interval", "5000");
             imageSlide.setAttribute("style", "background-color: #190f27;");
+
             const productImage = document.createElement("img");
             productImage.className = "productSlide productImage";  
             productImage.setAttribute("referrerPolicy", "no-referrer"); // Enables loading of google user content images 
             productImage.setAttribute("src", `images/products/${imageList[2][currentSlide]}`);
             productImage.setAttribute("height", "700");
+
             const dotImage = document.createElement("img")
             dotImage.className = "productSlide dot";
             dotImage.setAttribute("src", "images/blurredDot.png")
+
             const moneyDot = document.createElement("img")
             moneyDot.className = "productSlide moneyDot"; 
             moneyDot.setAttribute("src", "images/vibrantDot.png")
+
             const imageText = document.createElement("div");
             imageText.className =  "carousel-caption d-none d-md-block productPrice";
+
             const product = document.createElement("p");
             product.className = "itemText";
+
             const productNode = document.createTextNode(imageList[0][currentSlide]);
             product.append(productNode) 
+
             const price = document.createElement("p");
             price.className = "price";
             const priceNode = document.createTextNode(imageList[3][currentSlide]);
             price.append(priceNode);
+            
             imageText.append(product);
             imageText.append(price);
             imageSlide.append(productImage);
